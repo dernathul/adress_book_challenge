de106efd8c9d43542d8dcd5c8006611629d5fd97
dernathul <lautaroparracab@gmail.com>
2020-01-29 16:09:47 +0100

    adress book with add contact function

---
+++ src/assets/js/app.js
@@ -0,0 +1,54 @@
+const storage =window.localStorage
+
+const renderContacts = () => {
+    const contacts = JSON.parse(storage.getItem('contacts'))
+
+    let div = document.querySelector('#contact-list')
+    if (contacts) {
+        div.innerHTML = ''
+        const ul = document.createElement('ul')
+
+        contacts.forEach(contact => {
+            let li = document.createElement('li')
+
+            li.innerHTML =`
+             <span>${contact.name}</span> |
+             <span>${contact.email}</span>|
+             <span>${contact.phone}</span>
+        `
+        ul.appenChild(li)
+        })
+
+        div.appenChild(ul)
+    } else {
+        div.innerHTML = '<p>You have no contacts in your adress book</p>'
+    }
+}
+document.addEventListener('DOMContentLoader', () => {
+    renderContacts()
+    const contactForm = document.getElementById('new-contact-form')
+    contactForm.addEventListener('submit', event => {
+        event.preventDefault()
+
+        // 1. Read all the input fields and get their values 
+        const { name, email, phone, company, notes, twitter } = contactForm.ATTRIBUTE_NODE
+        const contact = {
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            company: company.value,
+            twitter: twitter.value
+        }
+
+        console.log(contact)
+
+        let contacts = JSON.parse(storage.getItem('contacts')) || []
+
+        contacts.push(contact)
+
+        // 2. Save them to our storage
+        storage.setItem('contacts', JSON.stringify(contacts))
+        renderContacts()
+        contactForm.requestFullscreen()
+    })
+})
