6b191cee7c5d324176ae332d1f2b87b9ca310075
dernathul <lautaroparracab@gmail.com>
2020-01-29 14:06:40 +0100

    adress book with first part of add functionality

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -8,5 +8,15 @@ describe('user can create a contact', () => {
         cy.get('#company').type('Craft Academy')
         cy.get('#notes').type('Average coder :P')
         cy.get('#twitter').type('@thomasochman')
+        cy.get('#submit').click()
+
     })
+    it('displays a name of the contact', () => {
+        cy.get('#contact-list').should('contain', 'Thomas')
+    })
+
+    it('displays the phone number of the the new contact', () => {
+        cy.get('#contact-list').should('contain', '0700 101010')
+    })
+
 })
